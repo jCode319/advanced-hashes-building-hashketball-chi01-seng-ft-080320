f409106ddd89b8309ee947f2009beca11f23025b
Jarrell Jackson <jdjack3@gmail.com>
2020-07-24 01:22:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,32 @@
 require'pry'
 
 def hasketball
-  game_hash = {}
+  game_hash = {
+    :home =>{
+      :team_name => "Brooklyn Nets"
+      :colors =>["Black", "White"]
+      :players => [
+        { :player_name = 
+          :number
+          :shoe
+          :points
+          :rebounds
+          :assists
+          :steals
+          :blocks
+          :slam_dunks
+          
+        }
+      ]
+    }
+
+
+
+    :away
+
+
+
+
+    
+  }
 end
